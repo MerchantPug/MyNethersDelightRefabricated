e86201069441d7ef97f27d68a81545bbceecd98a
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2024-11-08 01:37:25 +1100

    Fix #1.

--- src/main/java/com/soytutta/mynethersdelight/common/utility/MNDSoilUtils.java
+++ src/main/java/com/soytutta/mynethersdelight/common/utility/MNDSoilUtils.java
@@ -1,29 +1,27 @@
 package com.soytutta.mynethersdelight.common.utility;
 
+import com.soytutta.mynethersdelight.common.tag.MNDTags;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.CropBlock;
-import net.minecraft.world.level.block.NetherWartBlock;
 import net.minecraft.world.level.block.PitcherCropBlock;
-import net.minecraft.world.level.block.WaterlilyBlock;
-import vectorwing.farmersdelight.common.tag.ModTags;
 
 public class MNDSoilUtils {
     public static boolean isAbleToPlaceResurgentSoil(Block block) {
-        if (block.builtInRegistryHolder().is(ModTags.DOES_NOT_SURVIVE_RICH_SOIL))
+        if (block.builtInRegistryHolder().is(MNDTags.DOES_NOT_SURVIVE_RESURGENT_SOIL))
             return false;
 
-        if (block.builtInRegistryHolder().is(ModTags.SURVIVES_RICH_SOIL_FARMLAND))
+        if (block.builtInRegistryHolder().is(MNDTags.SURVIVES_RESURGENT_SOIL))
             return true;
 
         return !(block instanceof CropBlock || block instanceof PitcherCropBlock);
     }
 
     public static boolean isAbleToPlaceResurgentSoilFarmland(Block block) {
-        if (block.builtInRegistryHolder().is(ModTags.DOES_NOT_SURVIVE_RICH_SOIL_FARMLAND))
+        if (block.builtInRegistryHolder().is(MNDTags.DOES_NOT_SURVIVE_RESURGENT_SOIL_FARMLAND))
             return false;
 
-        if (block.builtInRegistryHolder().is(ModTags.SURVIVES_RICH_SOIL_FARMLAND))
+        if (block.builtInRegistryHolder().is(MNDTags.DOES_NOT_SURVIVE_RESURGENT_SOIL_FARMLAND))
             return true;
 
         return !(block == Blocks.DEAD_BUSH || block == Blocks.LILY_PAD || block == Blocks.RED_MUSHROOM || block == Blocks.BROWN_MUSHROOM);
